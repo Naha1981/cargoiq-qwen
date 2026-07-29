0d36946779d7a1e7f31acda7661711b65a435611
NahaLabs <nahat@nahalabs.co.za>
2026-07-29 07:48:08 +0200

    chore: restyle commits for all sub-passes (+ .gitignore)

--- src/app/signup/[[...rest]]/page.tsx
+++ src/app/signup/[[...rest]]/page.tsx
@@ -5,7 +5,7 @@ import { Logo } from '@/components/ui/logo';
 
 export default function SignupPage() {
   return (
-    <div className="min-h-screen bg-[#1A2332] flex flex-col items-center justify-center px-4">
+    <div className="min-h-screen bg-surface-container-lowest flex flex-col items-center justify-center px-4">
       <div className="mb-8">
         <Logo size="lg" dark />
       </div>
